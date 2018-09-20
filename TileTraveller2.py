a0550c799119c98aeed4f10a9d07c83b64749b84
teklakristjans <42963690+teklakristjans@users.noreply.github.com>
2018-09-20 16:09:59 +0000

    Check Victory

---
+++ TileTraveller2.py
@@ -0,0 +1,12 @@
+pos = 1.1
+
+def check_victory(p):
+    if p == 3.1:
+        print("Victory!")
+        quit()
+        
+        
+while True:
+    round_pos = round(pos, 1)
+
+    check_victory(round_pos)
